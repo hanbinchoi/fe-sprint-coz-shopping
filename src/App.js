b7c67521156ab590d891eed07bc4b4f2cfb673a2
hanbinchoi <wkghskak@naver.com>
2023-07-11 17:07:12 +0900

    Feat: Header - 메뉴 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import Header from "./components/Header";
-
 function App() {
+  const [isDropdownView, setDropDownView] = useState(false);
   return (
     <>
-      <Header></Header>
+      <Header
+        isDropdownView={isDropdownView}
+        setDropDownView={setDropDownView}
+      ></Header>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -17,15 +17,56 @@ const Logo = styled.div`
     font-size: 32px;
   }
 `;
-function Header() {
+const Menu = styled.div`
+  padding: 30px 78px;
+  font-size: 3rem;
+`;
+const DropDownMenu = styled.ul`
+  position: absolute;
+  right: 50px;
+  width: 200px;
+  background-color: white;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  border-radius: 20px;
+  li {
+    width: 200px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 16px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  }
+
+  li:last-child {
+    border-bottom: none;
+  }
+`;
+function Header({ isDropdownView, setDropDownView }) {
+  const handleClickContainer = (e) => {
+    e.stopPropagation();
+    if (e.currentTarget.nodeName === "svg") setDropDownView(!isDropdownView);
+    else {
+      setDropDownView(false);
+    }
+  };
   return (
-    <Container>
+    <Container onClick={handleClickContainer}>
       <Logo>
         <img src={logo} alt="로고" />
         <span>COZ Shopping</span>
       </Logo>
+      <Menu>
+        <BiMenu onClick={handleClickContainer} />
+        {isDropdownView ? (
+          <DropDownMenu>
+            <li>최한빈님, 안녕하세요!</li>
+            <li>상품리스트 페이지</li>
+            <li>북마크 페이지</li>
+          </DropDownMenu>
+        ) : null}
+      </Menu>
     </Container>
   );
 }
-
 export default Header;
