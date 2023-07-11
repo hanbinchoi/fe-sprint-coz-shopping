0edb8d4f2a8fb62956bc0c6fe3a68fea79e120e8
hanbinchoi <wkghskak@naver.com>
2023-07-11 18:58:49 +0900

    Feat: Main Page 뼈대

---
+++ src/components/Product.js
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  width: 264px;
+  height: 264px;
+  background-color: gray;
+`;
+
+function Product() {
+  return <Container>Product</Container>;
+}
+
+export default Product;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,40 @@
 import { styled } from "styled-components";
+import Product from "../components/Product";
 
 const Container = styled.div`
   padding: 104px 76px 88px;
   height: 640px;
+  div {
+    size: 24px;
+    font-weight: 600;
+    margin-bottom: 12px;
+  }
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 function Main() {
-  return <Container>Main</Container>;
+  return (
+    <Container>
+      <div>상품 리스트</div>
+      <ItemContainer>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </ItemContainer>
+      <div>북마크 리스트</div>
+      <ItemContainer>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </ItemContainer>
+    </Container>
+  );
 }
 
 export default Main;
